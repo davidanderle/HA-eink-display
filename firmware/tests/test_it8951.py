890b1ac2878b83b6c43117cc6c6ad82187843ca2
davidanderle <anderle.david@gmail.com>
2023-09-30 11:15:04 +0100

    Fine-tuned test methods

--- firmware/tests/test_it8951.py
+++ firmware/tests/test_it8951.py
@@ -1,32 +1,13 @@
+import struct
 import unittest
-from array import array
 from parameterized import parameterized
 from unittest.mock import Mock, patch
 import sys
+import os
 # Ensure that the parent directory is visible from this module
-sys.path.append('..')
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_root)
 from it8951 import it8951, Command
-
-def spi_write(data: bytearray):
-    #formatted_bytes = '-'.join([f'0x{byte:02X}' for byte in data])
-    #print(f'SPI MOSI: {formatted_bytes}')
-    txed_bytes.extend(data)
-
-def gpio_set_value(val: int):
-    #print("SPI nCS", val)
-    pass
-
-mock_spi = Mock()
-mock_spi.write.side_effect = spi_write
-
-mock_ncs = Mock()
-mock_ncs.side_effect = gpio_set_value
-
-# Pretend that the HRDY is high (IT8951 ready to accept commands)
-mock_hrdy = Mock()
-mock_hrdy.value.return_value = 1
-
-txed_bytes = bytearray()
 
 class test_it8951(unittest.TestCase):
     print("==[Running it8951 tests]==")
@@ -37,9 +18,11 @@ class test_it8951(unittest.TestCase):
         (Command.GET_DEV_INFO, [0x60, 0x00, 0x03, 0x02]),
     ])
     def test_send_command(self, command, expected_bytes):
-        tcon = it8951(mock_spi, mock_ncs, mock_hrdy)
-        tcon.send_command(command)
-        self.assertEqual(txed_bytes, bytes(expected_bytes))
+        tcon = it8951(self.mock_spi, self.mock_ncs, self.mock_hrdy)
+        self.assertEqual(self.ncs, 1)
+        tcon._send_command(command)
+        self.assertEqual(self.ncs, 1)
+        self.assertEqual(self.txed_bytes, expected_bytes)
     
     @parameterized.expand([
         ([0x0000, 0x0203, 0x4567], [0x00, 0x00, 0x00, 0x00, 0x02, 0x03, 0x45, 0x67]),
@@ -48,9 +31,11 @@ class test_it8951(unittest.TestCase):
         ([],                       [])
     ])
     def test_write_data(self, data, expected_bytes):
-        tcon = it8951(mock_spi, mock_ncs, mock_hrdy)
-        tcon.write_data(array('H', data))
-        self.assertEqual(txed_bytes, bytes(expected_bytes))
+        tcon = it8951(self.mock_spi, self.mock_ncs, self.mock_hrdy)
+        self.assertEqual(self.ncs, 1)
+        tcon._write_data(data)
+        self.assertEqual(self.ncs, 1)
+        self.assertEqual(self.txed_bytes, expected_bytes)
     
     @parameterized.expand([
         (0, [],                               []),
@@ -58,23 +43,46 @@ class test_it8951(unittest.TestCase):
         (4, [0x0000, 0x7FFF, 0x8000, 0xFFFF], [0x10,0,0,0,0,0,0,0,0,0,0,0])
     ])
     def test_read_data(self, len, expected_words, expected_tx):
-        def spi_write_readinto(txdata: bytearray, rxdata: bytearray):
-            u16_array = array('H', [0,0]+expected_words)
-            u16_array.byteswap()
-            rxdata[:] = u16_array.tobytes()
-            txed_bytes.extend(txdata)
-            #formatted_bytes = '-'.join([f'0x{byte:02X}' for byte in rxdata])
-            #print(f'SPI MISO: {formatted_bytes}')
-            #formatted_bytes = '-'.join([f'0x{byte:02X}' for byte in txdata])
-            #print(f'SPI MOSI: {formatted_bytes}')
+        expected_bytes = b''.join(struct.pack('>H', word) for word in [0, 0] + expected_words)
+        def spi_read(nbytes: int, txdata: bytearray) -> bytearray:
+            self.txed_bytes.extend(txdata)
+            ret = expected_bytes[self.rxcounter:self.rxcounter+2]
+            self.rxcounter += nbytes
+            return ret
 
-        mock_spi.write_readinto = spi_write_readinto
-        tcon = it8951(mock_spi, mock_ncs, mock_hrdy)
-        rxed_words = tcon.read_data(len)
+        self.mock_spi.read = spi_read
+        tcon = it8951(self.mock_spi, self.mock_ncs, self.mock_hrdy)
+        self.assertEqual(self.ncs, 1)
+        rxed_words = tcon._read_data(len)
+        self.assertEqual(self.ncs, 1)
         self.assertEqual(rxed_words, expected_words)
-        self.assertEqual(txed_bytes, bytes(expected_tx))
+        self.assertEqual(self.txed_bytes, expected_tx)
     
+    # This method is called before any tests are run 
+    @classmethod
+    def setUpClass(cls):
+        def spi_write(data: bytearray):
+            assert cls.ncs == 0, "nCS must be set low during SPI TxR"
+            cls.txed_bytes.extend(data)
+
+        def gpio_set_ncs(val: int):
+            cls.ncs = val
+
+        cls.mock_spi = Mock()
+        cls.mock_spi.write.side_effect = spi_write
+
+        cls.mock_ncs = Mock()
+        cls.mock_ncs.side_effect = gpio_set_ncs
+
+        # Pretend that the HRDY is high (IT8951 ready to accept commands)
+        cls.mock_hrdy = Mock()
+        cls.mock_hrdy.value.return_value = 1
+
+        cls.txed_bytes = []
+        cls.ncs = 1
+        
+    # This method is called before each test. Ensure that the buffer is clear before each test
     def setUp(self) -> None:
-        # Ensure that the buffer is clear before each test
-        txed_bytes.clear()
+        self.txed_bytes.clear()
+        self.rxcounter = 0
         return super().setUp()
