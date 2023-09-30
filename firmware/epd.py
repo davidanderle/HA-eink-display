81e0877b368decc296cd636827c0ab1d2b7f9db8
davidanderle <anderle.david@gmail.com>
2023-09-30 14:48:46 +0100

    Adding init

--- firmware/epd.py
+++ firmware/epd.py
@@ -1,6 +1,15 @@
 from it8951 import it8951, Register
 
 class epd:
-    def __init__(tcon: it8951, self, width: int, height: int):
-        # TODO: Continue
-        tcon.write_reg(Register.BGVR)
+    def __init__(self, tcon: it8951, width: int, height: int, vcom_mV: int):
+        self.device_info = tcon.get_device_info()
+        print(self.device_info)
+
+        rxvcom_mV = tcon.get_vcom()
+        print(f"Current VCOM = {rxvcom_mV/1000}")
+
+        if vcom_mV != rxvcom_mV:
+            print(f"Settig VCOM to new value: {vcom_mV/1000}")
+            tcon.set_vcom(vcom_mV)
+            rxvcom_mV = tcon.get_vcom()
+            print("Success" if tcon.get_vcom() == vcom_mV else "Failed")
