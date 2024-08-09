773e0aaf6e4cde1a1b0d9f594017ae6f757652b0
davidanderle <anderle.david@gmail.com>
2024-08-09 16:06:42 +0100

    Setting up for LVGL. Project now builds!

--- src/main.c
+++ src/main.c
@@ -1,58 +1,58 @@
 #include <limits.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "lvgl.h"
-#include "driver/gpio.h"
-#include "driver/spi_master.h"
 #include "ble.h"
-#include "it8951.h"
 #include "esp_log.h"
+#include "lvgl.h"
+#include "display.h"
+#include "ui.h"
 
-static spi_device_handle_t spi = {
-};
+static void display_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
+    const uint32_t w = lv_area_get_width(area);
+    const uint32_t h = lv_area_get_height(area);
 
-static gpio_num_t ncs = 34;
-static gpio_num_t hrdy = 9;
-
-static inline bool it8951_transcieve(const void *txdata, void *rxdata, size_t len) {
-    return spi_device_transmit(spi, &(spi_transaction_t) {
-        .length = len*8,
-        .tx_buffer = txdata,
-        .rx_buffer = rxdata
-    }) == ESP_OK;
-}
-
-static inline void it8951_set_ncs(bool state) {
-    gpio_set_level(ncs, state);
-}
-
-static inline bool it8951_get_hrdy(void) {
-    return gpio_get_level(hrdy);
+    // TODO: Shift out the pixels to the it8951
+    
+    // This function must be called when the display has been updated
+    lv_disp_flush_ready(disp);
 }
 
 // TODO: Try to speed up compilation by removing unnecessary components
 // TODO: The serial output returns "ESP-IDF: 5.2.1", while the latest ESP-IDF is
 // 5.3 for the S3... Why is this the case? PIO seems updated
 void app_main(void) {
+    //1872x1404
+    // No need to worry about double-buffering as here FPS is not important...
+    static lv_color_t draw_buff[1024];
+
+    ESP_LOGI("CHIP_INFO", "PSRAM: %dMB", heap_caps_get_total_size(MALLOC_CAP_SPIRAM) / (1024 * 1024));
     ESP_LOGI("HA-EINK", "Starting HA E-Ink display...");
     
     ble_init();
 
-    //stIT8951_Handler_t hdlr = {
-    //    .spi_transcieve = it8951_transcieve,
-    //    .set_ncs        = it8951_set_ncs,
-    //    .get_hrdy       = it8951_get_hrdy,
-    //    .vcom_mv        = INT_MAX,
-    //};
-    //it8951_init(&hdlr);
+    display_init();
 
-    //lv_init();
-    //lv_obj_t *label = lv_label_create(lv_scr_act());
-    //lv_label_set_text(label, "Hello World!");
-    ESP_LOGI("CHIP_INFO", "PSRAM: %dMB", heap_caps_get_total_size(MALLOC_CAP_SPIRAM) / (1024 * 1024));
+    lv_init();
 
-    // Enter a 1s background loop (this is not required if the setup is complete)
+    // TODO: May need to set the tick callback for this
+    //lv_tick_set_cb();
+
+    // Create the display and attach the displaying function
+    lv_display_t *disp = lv_display_create(1404, 1872);
+    lv_display_set_flush_cb(disp, display_flush);
+    // No need to worry about double-buffering as here the FPS is not important
+    lv_display_set_buffers(disp, draw_buff, NULL, sizeof(draw_buff), LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+    ui_init();
+
+    // TODO: May want to create a task for the GUI
+    //xTaskCreatePinnedToCore(guiTask, "gui", 8192, NULL, 0, NULL, 1);
+
+    // Enter a 1ms background loop (this is not required if the setup is complete)
     while(true) {
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        lv_task_handler();
+        vTaskDelay(1/portTICK_PERIOD_MS);
     }
+
+    lv_deinit();
 }
