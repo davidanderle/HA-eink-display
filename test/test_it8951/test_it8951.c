a2a3321bff47e756618cd981e8c5a60fc604ac2d
davidanderle <anderle.david@gmail.com>
2024-08-11 11:11:34 +0100

    Testing away...

--- test/test_it8951/test_it8951.c
+++ test/test_it8951/test_it8951.c
@@ -3,17 +3,14 @@
 #include <stdbool.h>
 #include "esp_log.h"
 #include "unity.h"
-#include "unity_fixture.h"
 #include "it8951.h"
 #include "test_it8951.h"
 
-// TODO: Need to define UNIT_TEST in the test build!
-
 // Expose the low-level testee functions from the driver
-// TODO: Changed last argument to int32_t!
-extern bool send_with_preamble(stIT8951_Handler_t *hdlr, const eIT8951_SpiPreamble_t preamble, const uint16_t *const data, const int32_t count);
-extern bool write_bytes(stIT8951_Handler_t *hdlr, const uint8_t *const data, const uint32_t count);
-extern bool read_data(stIT8951_Handler_t *hdlr, uint16_t *const data, const uint32_t count);
+extern bool send_command(stIT8951_Handler_t *hdlr, const eIT8951_Command_t cmd);
+extern bool write_bytes(stIT8951_Handler_t *hdlr, const uint8_t *const data, const int32_t count);
+extern bool read_data(stIT8951_Handler_t *hdlr, uint16_t *const data, const int32_t count);
+extern bool write_data(stIT8951_Handler_t *hdlr, const uint16_t *const data, const int32_t count);
 
 /// @brief Used to store the current state of the nCS pin
 static bool ncs = 1;
@@ -25,8 +22,12 @@ static bool mock_spi_transcieve(const void *tx, void *rx, size_t len) {
         printf("TEST: rxdata buffer cannot be NULL\n");
         return false;
     } else {
+        TEST_ASSERT_FALSE(ncs);
         memcpy(&_txdata[_txcount], tx, len);
         _txcount += len;
+        for(uint32_t i=0; i<len; i++){
+            ESP_LOGI("TEST_SPI", "0x%02x", ((uint8_t*)tx)[i]);
+        }
         return true;
     }
 }
@@ -52,38 +53,58 @@ void suiteSetUp(void) {
     printf("==[ Testing IT8951 driver... ]==\n");
 }
 
-void test_send_with_preamble(uint16_t preamb, uint16_t data, uint8_t *expected_tx) {
-    _txdata = calloc(sizeof(preamb)+sizeof(data), sizeof(uint8_t));
+void test_send_command(const eIT8951_Command_t cmd, uint8_t *expected_tx) {
+    const size_t size = (1+1)*sizeof(uint16_t);
+    _txdata = calloc(size, sizeof(uint8_t));
     _txcount = 0;
     if(_txdata) {
         TEST_ASSERT_TRUE(ncs);
-        TEST_ASSERT_TRUE(send_with_preamble(&hdlr, preamb, &data, 1));
+        TEST_ASSERT_TRUE(send_command(&hdlr, cmd));
         TEST_ASSERT_TRUE(ncs);
-        ESP_LOGI("TEST", "TX'd [0x%02x 0x%02x 0x%02x 0x%02x]\n", 
-                 _txdata[0], _txdata[1], _txdata[2], _txdata[3]);
-        TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_tx, _txdata, sizeof(preamb)+sizeof(data));
+        TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_tx, _txdata, size);
         free(_txdata);
     } else {
         printf("TEST: Insufficient memory!\n");
     }
 }
+void test_send_command_multiple_args(void) {
+    test_send_command(IT8951_COMMAND_SYS_RUN,      (uint8_t[]){0x60, 0x00, 0x00, 0x01});
+    test_send_command(IT8951_COMMAND_STANDBY,      (uint8_t[]){0x60, 0x00, 0x00, 0x02});
+    test_send_command(IT8951_COMMAND_CMD_VCOM,     (uint8_t[]){0x60, 0x00, 0x00, 0x39});
+    test_send_command(IT8951_COMMAND_GET_DEV_INFO, (uint8_t[]){0x60, 0x00, 0x03, 0x02});
+}
 
-void test_send_with_preamble_multiple_args(void) {
-    test_send_with_preamble(IT8951_SPI_PREAMBLE_COMMAND, IT8951_COMMAND_SYS_RUN,
-                           (uint8_t[]){0x60, 0x00, 0x00, 0x01});
-    test_send_with_preamble(IT8951_SPI_PREAMBLE_COMMAND, IT8951_COMMAND_STANDBY,
-                           (uint8_t[]){0x60, 0x00, 0x00, 0x02});
-    test_send_with_preamble(IT8951_SPI_PREAMBLE_COMMAND, IT8951_COMMAND_CMD_VCOM,
-                           (uint8_t[]){0x60, 0x00, 0x00, 0x39});
-    test_send_with_preamble(IT8951_SPI_PREAMBLE_COMMAND, IT8951_COMMAND_GET_DEV_INFO,
-                           (uint8_t[]){0x60, 0x00, 0x03, 0x02});
+void test_write_data(const uint16_t *const data, int32_t count, uint8_t *expected_tx) {
+    const size_t size = (1+count)*sizeof(uint16_t);
+    _txdata = calloc(size, sizeof(uint8_t));
+    _txcount = 0;
+    if(_txdata) {
+        TEST_ASSERT_TRUE(ncs);
+        TEST_ASSERT_TRUE(write_data(&hdlr, data, count));
+        TEST_ASSERT_TRUE(ncs);
+        TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_tx, _txdata, size);
+        free(_txdata);
+    } else {
+        printf("TEST: Insufficient memory!\n");
+    }
+}
+void test_write_data_multiple_args(void) {
+    test_write_data((uint16_t[]){0x0000, 0x0203, 0x8567}, 3, 
+                    (uint8_t []){0x00, 0x00, 0x00, 0x00, 0x02, 0x03, 0x85, 0x67});
+    test_write_data((uint16_t[]){0x7FFF}, 1,
+                    (uint8_t []){0x00, 0x00, 0x7F, 0xFF});
+    test_write_data((uint16_t[]){0xFFFF}, 1,
+                    (uint8_t []){0x00, 0x00, 0xFF, 0xFF});
+    test_write_data((uint16_t[]){}, 0, (uint8_t[]){});
+}
 }
 
 // This is required for the ESP-IDF framework
 void app_main() {
     UNITY_BEGIN();
 
-    RUN_TEST(test_send_with_preamble_multiple_args);
+    RUN_TEST(test_send_command_multiple_args);
+    RUN_TEST(test_write_data_multiple_args);
 
     UNITY_END();
 }
