aba9806c7d7bb8c6da6dafc74aaeb5cc9f6f0c31
davidanderle <anderle.david@gmail.com>
2024-08-14 21:16:20 +0100

    Changed bit depth to 8

--- inc/lv_conf.h
+++ inc/lv_conf.h
@@ -29,7 +29,7 @@
  *====================*/
 
 /*Color depth: 8 (A8), 16 (RGB565), 24 (RGB888), 32 (XRGB8888)*/
-#define LV_COLOR_DEPTH 16
+#define LV_COLOR_DEPTH 8
 
 /*=========================
    STDLIB WRAPPER SETTINGS
