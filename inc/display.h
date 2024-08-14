8fe0203c235561d3e0a167b573891d078c2dfd13
davidanderle <anderle.david@gmail.com>
2024-08-14 21:17:08 +0100

    Added display spec

--- inc/display.h
+++ inc/display.h
@@ -1,6 +1,9 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#define DISPLAY_HOR_RES (1872)
+#define DISPLAY_VER_RES (1404)
+
 void display_init(void);
 void display_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map);
 
