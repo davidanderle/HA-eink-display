e3f2b422add0ba653b1f2a0e679ad4975daadea0
davidanderle <anderle.david@gmail.com>
2024-08-18 18:04:42 +0100

    Adding unfinished tests

--- test/test_it8951/test_it8951.c
+++ test/test_it8951/test_it8951.c
@@ -18,15 +18,18 @@ extern bool write_data(stIT8951_Handler_t *hdlr, const uint16_t *const data, con
 static bool ncs = 1;
 static uint8_t *_txdata;
 static uint16_t *_rxdata;
-static int _txcount = 0;
+static uint32_t _txcount = 0;
+static uint32_t _cnt = 0;
 
-// TODO: Should be able to do the _txdata allocation in the setup?
+// TODO: These tests should be rewritten with parametric testing (see Claude) 
 
 static bool mock_spi_transcieve(const void *tx, void *rx, size_t len) {
     // This big-endian buffer is simulateing data returned by the IT8951. Note:
     // the first 4 bytes are dummy and should be ignored by the processing func
     static const uint8_t rxdata[] = {0x00,0x00,0x00,0x00,0x12,0x34,0x00,0x00,0x7F,0xFF,0x80,0x00,0xFF,0xFF};
     assert(len <= sizeof(rxdata));
+
+    _cnt++;
 
     if(!_txdata) {
         ESP_LOGE(tag, "txdata buffer cannot be NULL");
@@ -73,12 +76,13 @@ void suiteSetUp(void) {
 void test_send_command(const eIT8951_Command_t cmd, uint8_t *expected_tx) {
     const size_t size = (1+1)*sizeof(uint16_t);
     _txdata = calloc(size, sizeof(*_txdata));
-    _txcount = 0;
+    _txcount = _cnt =  0;
     if(_txdata) {
         TEST_ASSERT_TRUE(ncs);
         TEST_ASSERT_TRUE(send_command(&hdlr, cmd));
         TEST_ASSERT_TRUE(ncs);
         TEST_ASSERT_EQUAL_HEX8_ARRAY(expected_tx, _txdata, size);
+        TEST_ASSERT_EQUAL(size/2, _cnt);
         free(_txdata);
     } else {
         printf("TEST: Insufficient memory!\n");
@@ -94,12 +98,13 @@ void test_send_command_multiple_args(void) {
 void test_write_data(const uint16_t *const data, int32_t count, uint8_t *expected_tx) {
     const size_t size = (1+count)*sizeof(uint16_t);
     _txdata = calloc(size, sizeof(*_txdata));
-    _txcount = 0;
+    _txcount = _cnt =  0;
     if(_txdata) {
         TEST_ASSERT_TRUE(ncs);
         TEST_ASSERT_TRUE(write_data(&hdlr, data, count));
         TEST_ASSERT_TRUE(ncs);
         TEST_ASSERT_EQUAL_HEX8_ARRAY(expected_tx, _txdata, size);
+        TEST_ASSERT_EQUAL(size/2, _cnt);
         free(_txdata);
     } else {
         ESP_LOGE(tag, "Insufficient memory!");
@@ -119,11 +124,12 @@ void test_read_data(const int32_t count, uint8_t *expected_tx, uint16_t *expecte
     const size_t size = (2+count)*sizeof(uint16_t);
     _txdata = calloc(size, sizeof(*_txdata));
     _rxdata = calloc(count, sizeof(*_rxdata));
-    _txcount = 0;
+    _txcount = _cnt =  0;
     if(_txdata && (_rxdata || count == 0)) {
         TEST_ASSERT_TRUE(ncs);
         TEST_ASSERT_TRUE(read_data(&hdlr, _rxdata, count));
         TEST_ASSERT_TRUE(ncs);
+        TEST_ASSERT_EQUAL(size/2, _cnt);
         if(count != 0){
             TEST_ASSERT_EQUAL_HEX8_ARRAY(expected_tx, _txdata, size);
             TEST_ASSERT_EQUAL_HEX16_ARRAY(expected_rx, _rxdata, count);
@@ -143,6 +149,61 @@ void test_read_data_multiple_args(void) {
                       (uint16_t[]){0x1234, 0x0000, 0x7FFF, 0x8000, 0xFFFF});
 }
 
+void test_pack_pixels(eIT8951_ColorDepth_t bpp, stRectangle_t *rect, uint16_t *expected_out, uint32_t count){
+    const uint32_t area = rectangle_get_area(rect);
+    uint8_t *in_pixels = malloc(area*sizeof(*in_pixels));
+    memset(in_pixels, 0x0F, area);
+    // TODO: 4 is 16bit/4bpp ->4pix/word
+    uint16_t *packed_pixels = malloc((rect->width/4+2)*rect->height*sizeof(uint16_t));
+
+    if(packed_pixels && in_pixels) {
+        uint32_t cnt;
+        it8951_pack_pixels(&(stIT8951_ImageInfo_t){
+            .endianness = IT8951_ENDIANNESS_LITTLE,
+            .bpp = bpp,
+            .rotation = IT8951_ROTATION_MODE_0
+        }, rect, in_pixels, packed_pixels, &cnt);
+        TEST_ASSERT_EQUAL_UINT32(count, cnt);
+        TEST_ASSERT_EQUAL_HEX16_ARRAY(expected_out, packed_pixels, count);
+        if(count == 1)
+            ESP_LOGI(tag, "%x", packed_pixels[0]);
+        else if(count == 2)
+            ESP_LOGI(tag, "%x,%x", packed_pixels[0], packed_pixels[1]);
+        else if(count == 4)
+            ESP_LOGI(tag, "%x,%x,%x,%x", packed_pixels[0], packed_pixels[1],packed_pixels[2], packed_pixels[3]);
+    } else {
+        ESP_LOGE(tag, "Insufficient memory!");
+    }
+    free(in_pixels);
+    free(packed_pixels);
+}
+
+void test_pack_pixels_multiple_args(void) {
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 0, 4, 1}, (uint16_t[]){0xFFFF}, 1),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){3, 0, 4, 1}, (uint16_t[]){0xF000, 0x0FFF}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 0, 1, 4}, (uint16_t[]){0x000F,0x000F,0x000F,0x000F}, 4),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 0, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 1, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 2, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){0, 3, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2),
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){1, 0, 2, 2}, (uint16_t[]){0x0FF0, 0x0FF0}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){1, 1, 2, 2}, (uint16_t[]){0x0FF0, 0x0FF0}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){1, 2, 2, 2}, (uint16_t[]){0x0FF0, 0x0FF0}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){1, 3, 2, 2}, (uint16_t[]){0x0FF0, 0x0FF0}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){2, 0, 2, 2}, (uint16_t[]){0xFF00, 0xFF00}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){2, 1, 2, 2}, (uint16_t[]){0xFF00, 0xFF00}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){2, 2, 2, 2}, (uint16_t[]){0xFF00, 0xFF00}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){2, 3, 2, 2}, (uint16_t[]){0xFF00, 0xFF00}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){3, 0, 2, 2}, (uint16_t[]){0xF000, 0x000F, 0xF000, 0x000F}, 4);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){3, 1, 2, 2}, (uint16_t[]){0xF000, 0x000F, 0xF000, 0x000F}, 4);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){3, 2, 2, 2}, (uint16_t[]){0xF000, 0x000F, 0xF000, 0x000F}, 4);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){3, 3, 2, 2}, (uint16_t[]){0xF000, 0x000F, 0xF000, 0x000F}, 4);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){4, 0, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){4, 1, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){4, 2, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2);
+    test_pack_pixels(IT8951_COLOR_DEPTH_BPP_4BIT, &(stRectangle_t){4, 3, 2, 2}, (uint16_t[]){0x00FF, 0x00FF}, 2);
+}
+
 // This is required for the ESP-IDF framework
 void app_main() {
     UNITY_BEGIN();
@@ -150,6 +211,7 @@ void app_main() {
     RUN_TEST(test_send_command_multiple_args);
     RUN_TEST(test_write_data_multiple_args);
     RUN_TEST(test_read_data_multiple_args);
+    //RUN_TEST(test_pack_pixels_multiple_args);
 
     UNITY_END();
 }
