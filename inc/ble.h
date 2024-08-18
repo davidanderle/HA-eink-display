4787a98785fb72e39e0690fd0673256b38853614
davidanderle <anderle.david@gmail.com>
2024-08-18 17:58:33 +0100

    Max size of a BLE message limit added

--- inc/ble.h
+++ inc/ble.h
@@ -1,6 +1,8 @@
 #ifndef H_BLE
 #define H_BLE
 
+#define MAX_BLE_MSG_SIZE (64*1024) //64kB
+
 // TODO: Surely this is in the ESP-IDF libraries somewhere...
 // Standard characteristic user description descriptor UUID
 #define BLE_UUID_DESC_CUSTOM_CHAR_NAME (0x2901) 
